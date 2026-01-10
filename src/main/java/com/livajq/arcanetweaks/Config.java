985769050b10fdcb0c392f54bdcd119b3dc3ae95
LivajQ <leviackerman.pl@gmail.com>
2026-01-10 20:52:44 +0100

    Dread mob allies list

--- src/main/java/com/livajq/arcanetweaks/Config.java
+++ src/main/java/com/livajq/arcanetweaks/Config.java
@@ -5,22 +5,37 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @Mod.EventBusSubscriber(modid = ArcaneTweaks.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Config {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
     
     //#####################################################################//
     
-    private static final ForgeConfigSpec.IntValue MAGIC_NUMBER = BUILDER
-            .comment("A magic number")
-            .defineInRange("magicNumber", 42, 0, Integer.MAX_VALUE);  //dummy for generating the file
+    private static final ForgeConfigSpec.ConfigValue<List<? extends String>> EXTRA_ALLIES =
+            BUILDER.comment("Entities treated as allies by dread mobs")
+                    .defineListAllowEmpty(
+                            List.of("dreadMobAllies"),
+                            List.of(
+                                    "minecraft:villager",
+                                    "minecraft:zombie"
+                            ),
+                            o -> o instanceof String
+                    );
     
     //#####################################################################//
     
     static final ForgeConfigSpec SPEC = BUILDER.build();
     
+    
+    
+    public static Set<String> extraAlliesSet = new HashSet<>();
+    
     @SubscribeEvent
     static void onLoad(final ModConfigEvent event) {
-    
+        extraAlliesSet = new HashSet<>(EXTRA_ALLIES.get());
     }
 }
---
+++ src/main/java/com/livajq/arcanetweaks/handlers/IceAndFireHandler.java
@@ -0,0 +1,55 @@
+package com.livajq.arcanetweaks.handlers;
+
+import com.github.alexthe666.iceandfire.entity.util.IDreadMob;
+import com.livajq.arcanetweaks.ArcaneTweaks;
+import com.livajq.arcanetweaks.Config;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingChangeTargetEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
+
+@Mod.EventBusSubscriber(modid = ArcaneTweaks.MODID)
+public class IceAndFireHandler {
+    
+    //stop dread mobs from targeting allies
+    @SubscribeEvent
+    public static void onChangeTarget(LivingChangeTargetEvent event) {
+        LivingEntity attacker = event.getEntity();
+        LivingEntity newTarget = event.getNewTarget();
+        if (newTarget == null || !(attacker instanceof IDreadMob)) return;
+        
+        if (isDreadMobAlly(newTarget)) event.setCanceled(true);
+    }
+    
+    //cancel dread mob and allied mob friendly fire
+    @SubscribeEvent
+    public static void onLivingAttack(LivingAttackEvent event) {
+        Entity attacker = event.getSource().getEntity();
+        LivingEntity victim = event.getEntity();
+        
+        if (attacker == null || victim == null) return;
+        
+        boolean attackerIsDread = attacker instanceof IDreadMob;
+        boolean victimIsDread = victim instanceof IDreadMob;
+        
+        if (attackerIsDread == victimIsDread) return;
+        
+        if (attackerIsDread && isDreadMobAlly(victim)) event.setCanceled(true);
+        else if (isDreadMobAlly((LivingEntity) attacker)) event.setCanceled(true);
+    }
+    
+    private static boolean isDreadMobAlly(LivingEntity entity) {
+        if (entity == null) return false;
+        
+        EntityType<?> type = entity.getType();
+        ResourceLocation id = ForgeRegistries.ENTITY_TYPES.getKey(type);
+        if (id == null) return false;
+        
+        return Config.extraAlliesSet.contains(id.toString());
+    }
+}
