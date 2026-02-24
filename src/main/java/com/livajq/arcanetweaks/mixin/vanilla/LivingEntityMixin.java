4acaeda29287b1c52cadf153ff24f335b49a46b4
LivajQ <leviackerman.pl@gmail.com>
2026-02-24 20:10:11 +0100

    Configurable resistance reduction

--- src/main/java/com/livajq/arcanetweaks/mixin/vanilla/LivingEntityMixin.java
+++ src/main/java/com/livajq/arcanetweaks/mixin/vanilla/LivingEntityMixin.java
@@ -1,9 +1,14 @@
 package com.livajq.arcanetweaks.mixin.vanilla;
 
+import com.livajq.arcanetweaks.Config;
 import com.livajq.arcanetweaks.interfaces.TickSpeedAccessor;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin implements TickSpeedAccessor {
@@ -19,5 +24,20 @@ public class LivingEntityMixin implements TickSpeedAccessor {
     @Override
     public void arcanetweaks$setTickSpeedAccumulator(double value) {
         arcanetweaks$tickSpeedAccumulator = value;
+    }
+    
+    @Redirect(
+            method = "getDamageAfterMagicAbsorb",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/lang/Math;max(FF)F"
+            )
+    )
+    private float redirectResCalc(float a, float b, DamageSource src, float pDamageAmount) {
+        LivingEntity self = (LivingEntity)(Object)this;
+        
+        final float resPerLvl = Config.resistanceAmount; //% reduction per resistance level
+        pDamageAmount -= pDamageAmount * (self.getEffect(MobEffects.DAMAGE_RESISTANCE).getAmplifier() + 1) * resPerLvl;
+        return Math.max(pDamageAmount, 0.0F);
     }
 }
