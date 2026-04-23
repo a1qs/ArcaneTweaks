1f8f64074cf6533251bb7e7df0c4248f83271bd8
a1qs <ankiller2007.09.20@gmail.com>
2026-04-23 20:38:50 +0200

    fix: SpartanWeaponry crash on dedicated servers

---
+++ src/main/java/com/livajq/arcanetweaks/mixin/spartanweaponry/MobMixinSquared.java
@@ -0,0 +1,48 @@
+package com.livajq.arcanetweaks.mixin.spartanweaponry;
+
+import com.bawnorton.mixinsquared.TargetHandler;
+import com.oblivioussp.spartanweaponry.mixin.LivingEntityMixin;
+import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.List;
+
+@Mixin(value = Mob.class, priority = 1500)
+public class MobMixinSquared extends LivingEntityMixin {
+
+    // Mixin into the Mixin from SpartansWeaponry
+    // For some reason, it complains about accessing a RandomSource from multiple threads, so we use the entity's RandomSource instead
+    @TargetHandler(
+            mixin = "com.oblivioussp.spartanweaponry.mixin.MobMixin",
+            name = "attemptReplacingMainHandItemRandom"
+    )
+    @Redirect(
+            method = "@MixinSquared:Handler",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/oblivioussp/spartanweaponry/util/ItemRandomizer;generate(Lnet/minecraft/world/level/Level;Ljava/util/List;)Lnet/minecraft/world/item/ItemStack;"
+            )
+    )
+    private ItemStack redirectItemRandomizer(Level level, List<Item> items) {
+        System.out.println("Redirected SpartanWeaponry mixin to use a fixed random");
+        return arcaneTweaks$generate(this.random, items);
+    }
+
+
+    @Unique
+    private static ItemStack arcaneTweaks$generate(RandomSource random, List<Item> items) {
+        float weaponRand = random.nextFloat();
+        float divider = 1.0F / (float)items.size();
+        int idx = Mth.floor(weaponRand / divider);
+        idx = idx > items.size() - 1 ? items.size() - 1 : idx;
+        return new ItemStack(items.get(idx));
+    }
+}
