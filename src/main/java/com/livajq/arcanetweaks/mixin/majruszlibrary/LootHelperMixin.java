2621b24fa95628512a1de9bc8e157dc998a6c60b
a1qs <ankiller2007.09.20@gmail.com>
2026-04-28 21:15:14 +0200

    fix: calling entity.level() when entity is null

--- src/main/java/com/livajq/arcanetweaks/mixin/majruszlibrary/LootHelperMixin.java
+++ src/main/java/com/livajq/arcanetweaks/mixin/majruszlibrary/LootHelperMixin.java
@@ -8,6 +8,8 @@ import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
+
+import net.minecraftforge.server.ServerLifecycleHooks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,11 +21,12 @@ public abstract class LootHelperMixin {
     @Inject(method = "toGiftParams", at = @At("HEAD"), cancellable = true, remap = false)
     private static void fixNullEntity(Entity entity, CallbackInfoReturnable<LootParams> cir) {
         if (entity == null) {
-            MinecraftServer server = entity.level().getServer();
+            MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
             if (server == null) return;
             
             ServerLevel level = server.overworld();
             Entity dummy = new Zombie(level);
+
             
             LootParams params = new LootParams.Builder(level)
                     .withParameter(LootContextParams.THIS_ENTITY, dummy)
